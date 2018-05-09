9f4c0aa99948ffac466b09a18b854c35eb36eb32
luwan <luwanobject@sina.com>
2018-05-09 17:04:07 +0800

    线程Demo文件

---
+++ src/main/java/com/luwan/watlockDemo.java
@@ -0,0 +1,90 @@
+package com.luwan;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by luwan on 2018/5/9.
+ */
+class Share{
+    private  int number=0;
+
+    Lock lock=new ReentrantLock();
+    private Condition condition=lock.newCondition();
+    public  void increat(){
+        lock.lock();
+        try {
+            //1、判断
+            while (number != 0) {
+                condition.await();
+            }
+            //2、干活
+            ++number;
+            System.out.println(Thread.currentThread().getName()+"\t"+number);
+            //3、通知
+            condition.signalAll();
+
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                lock.unlock();
+             }
+
+        }
+    public  void decreat(){
+        lock.lock();
+        try {
+            //1、判断
+            while (number == 0) {
+
+                condition.await();
+
+            }
+            //2、干活
+            --number;
+            System.out.println(Thread.currentThread().getName()+"\t"+number);
+            //3、通知
+            condition.signalAll();
+
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+}
+
+
+public class watlockDemo {
+    public static void main(String[] args) {
+        Share s=new Share();
+
+        new Thread(()->{
+            for (int i=0;i<10;i++){
+                s.increat();
+            }
+        },"AA"
+        ).start();
+        new Thread(()->{
+            for (int i=0;i<10;i++){
+                s.increat();
+            }
+        },"DD"
+        ).start();
+        new Thread(()->{
+            for (int i=0;i<10;i++){
+                s.decreat();
+            }
+        },"BB"
+        ).start();
+        new Thread(()->{
+            for (int i=0;i<10;i++){
+                s.decreat();
+            }
+        },"CC"
+        ).start();
+    }
+}
